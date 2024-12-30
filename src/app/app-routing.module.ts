7b5558127aa5bfad67710ea5c27f9be3d653d976
Polina Muravieva <polinamur3@gmail.com>
2024-12-31 02:33:51 +0300

    auth-forward guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LayoutComponent} from "./shared/layout/layout.component";
 import {MainComponent} from "./views/main/main.component";
+import {AuthForwardGuard} from "./core/auth/auth-forward.guard";
 
 const routes: Routes = [
   {
@@ -9,7 +10,7 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       {path: '', component: MainComponent},
-      { path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule) }
+      { path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule), canActivate: [AuthForwardGuard] }
     ]
   },
   {path: 'choice', redirectTo: ''}
