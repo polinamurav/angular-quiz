539eefb102dbf34e088510ec7f1cb7bd8d2677fe
Polina Muravieva <polinamur3@gmail.com>
2025-01-01 21:03:24 +0300

    redirect changed

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../core/auth/auth.service";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  link: string = '';
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.link = this.authService.getLoggedIn() ? '/choice' : '/signup';
   }
 
 }
